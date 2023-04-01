10d83f442ae5833b9c9639c198d579f99c38d403
ArthurKorablev <arttr86@gmail.com>
2023-04-01 23:42:44 +0300

    logo fixing 2

--- src/containers/MainLogin/index.js
+++ src/containers/MainLogin/index.js
@@ -1,11 +1,9 @@
 import './style.css';
 import Login from '../../components/Login';
-import Logo from '../../components/Logo';
 
 const MainLogin = () => {
   return (
     <div className="main">
-        {/* <Logo className = 'main-logo'/> */}
         <Login/>
     </div>
   );
